4dedd13ea3674bf8ef7abb28447ff8728725dde7
Pee8631 <thanabodee0206@gmail.com>
2025-03-03 16:59:30 +0700

    Add project files.

---
+++ QueueWebApi/Models/Queue.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QueueWebApi.Models
+{
+    public class Queue
+    {
+        [Key]
+        public int id { get; set; } = 0;
+        public string QueueNumber { get; set; } = string.Empty;
+        public DateTime createdAt { get; set; } = DateTime.MinValue;
+        public DateTime updatedAt { get; set; } = DateTime.MinValue;
+    }
+}
